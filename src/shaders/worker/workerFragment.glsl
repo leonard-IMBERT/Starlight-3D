d5f593710c5a7a6ff6a8da168f01d2c5a89295d6
Tigri <imbert418@gmail.com>
2019-12-03 13:33:02 +0100

    feat(shaders): Now display the map image in the offscreen canvas

--- src/shaders/worker/workerFragment.glsl
+++ src/shaders/worker/workerFragment.glsl
@@ -5,6 +5,5 @@ varying vec2 vTexturePosition;
 uniform sampler2D uTexture;
 
 void main() {
-  // gl_FragColor = texture2D(uTexture, vTexturePosition);
-  gl_FragColor = vec4(1.0, 0, 0, 1.0);
+  gl_FragColor = texture2D(uTexture, vTexturePosition);
 }
